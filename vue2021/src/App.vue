3a53551ee92b2bff107a8ce11f3d78797806bff4
dh_bgcuxj <webtekmasters@gmail.com>
2021-01-04 23:42:45 +0100

    Added Alt to the Header

--- vue2021/src/App.vue
+++ vue2021/src/App.vue
@@ -6,7 +6,7 @@
   </ul> -->
   <div>
     <a href="#">
-    <img class="h-8 w-auto sm:h-10" src="./assets/DAM.png" alt="">
+    <img class="h-8 w-auto sm:h-10" src="./assets/DAM.png" alt="Header Logo">
     </a>
   <!-- </div>
 <div> -->
